d69766be6fbde530cea038ec8298c49c1ab7a2af
onik-999 <onikdas.dev@gmail.com>
2026-03-08 15:16:07 +0600

    fixed issues & add all section

--- script/script.js
+++ script/script.js
@@ -1,171 +1,222 @@
-const allBTN = document.getElementById('allBtn')
+const allBTN = document.getElementById('all-btn')
 
-const openBTN = document.getElementById('openBtn')
+const openBTN = document.getElementById('open-btn')
 
-const closedBTN = document.getElementById('closedBtn')
+const closeBTN = document.getElementById('close-btn')
+
+const inputBTN = document.getElementById('input-search')
+
+const inputText = document.getElementById('input-text')
 
 const spinner = document.getElementById('spinner')
 const issueContainer = document.getElementById("issue-container");
-const issueCount = document.querySelector('.text-3xl.font-bold')
 
-const modal = document.getElementById('issueModal')
+const issueCount = document.getElementById('issue-count')
+const modal = document.getElementById('modal')
 
-const modalContainer = document.getElementById('modal-box') 
+const modalContainer = document.getElementById('modal-container')
 let allIssue = [];
-
-const showSpinner = () => {
-    spinner.classList.remove('hidden')
+const showSpinner =()=>{
+  spinner.classList.remove('hidden')
 }
-const hideSpinner = () => {
-    spinner.classList.add('hidden')
-}
-
-function activeButton(btn) {
-    allBTN.classList.remove("btn-primary")
-    openBTN.classList.remove("btn-primary")
-    closedBTN.classList.remove("btn-primary")
-    btn.classList.add("btn-primary")
+const hideSpinner =()=>{
+  spinner.classList.add('hidden')
 }
 
-allBTN.addEventListener('click', function () {
-    activeButton(allBTN)
-    displayIssue(allIssue)
+const searchValue= ()=>{
+  inputBTN.addEventListener('click',function(){
+activeButton(allBTN)
+showSpinner()
+ const inpuValue = inputText.value.trim().toLowerCase()
+
+ const url =`https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${inpuValue}`
+
+fetch(url)
+.then((res)=> res.json())
+.then((data)=> {
+  let value =  data.data
+
+  displayIssue(value)
+  
+})
+hideSpinner()
 })
 
-closedBTN.addEventListener('click', function () {
-    activeButton(closedBTN)
-    const closeIssue = allIssue.filter(issue => issue.status.toLowerCase() === "closed")
-    displayIssue(closeIssue)
+}
+ searchValue()
+
+
+function activeButton(btn){
+  allBTN.classList.remove("btn-primary")
+  openBTN.classList.remove("btn-primary")
+  closeBTN.classList.remove("btn-primary")
+
+  btn.classList.add("btn-primary")
+}
+allBTN.addEventListener('click',function(){
+activeButton(allBTN)
+  displayIssue(allIssue)
+})
+closeBTN.addEventListener('click',function(){
+ 
+  activeButton(closeBTN)
+  showSpinner()
+  const closeIssue = allIssue.filter(issue=> issue.status ==="closed")
+  displayIssue(closeIssue)
+  hideSpinner()
+
+})
+openBTN.addEventListener('click',function(){
+  activeButton(openBTN)
+  showSpinner()
+  const openIssue = allIssue.filter(issue=>issue.status==="open")
+  displayIssue(openIssue)
+  hideSpinner()
 })
 
-openBTN.addEventListener('click', function () {
-    activeButton(openBTN)
-    const openIssue = allIssue.filter(issue => issue.status.toLowerCase() === "open")
-    displayIssue(openIssue)
-})
+const loadModal =(id)=>{
 
-const loadModal = (id) => {
-  const url = `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`
+  
+const url = `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`
   fetch(url)
-  .then((res) => res.json())
-  .then((data) => {
-    displayModal(data.data)
-  })
+  .then((res)=>res.json())
+  .then((data)=>{
+   displayModal(data.data)})
+  }
+const displayModal=(info)=>{
+  const cardLabels = createLabels(info.labels);
+  modalContainer.innerHTML =`
+  <div class="w-full max-w-2xl bg-white rounded-xl ">
+    <div class="flex items-start justify-between">
+      <div>
+        <h2 class="text-xl font-semibold text-gray-800">
+        ${info.title}
+        </h2>
+
+        <div class="flex items-center gap-2 mt-2 text-sm text-gray-500">
+          <span class="px-2 py-0.5 text-xs font-medium rounded-full ${
+  info.status === "open"
+    ? "text-green-700 bg-green-100"
+    : "text-red-700 bg-red-100"
+} ">
+        ${info.status}
+          </span>
+          <span>• Opened ${info.author}</span>
+          <span>• ${info.createdAt}</span>
+        </div>
+      </div>
+    </div>
+    <div class="flex gap-2 mt-4">
+     ${cardLabels}
+    </div>
+    <p class="mt-4 text-gray-600 text-sm line-clamp-2">
+     ${info.description}
+    </p>
+    <div class="mt-6 bg-gray-100 rounded-lg p-4 grid grid-cols-2 gap-4">
+
+      <div>
+        <p class="text-xs text-gray-500">Assignee:</p>
+        <p class="font-medium text-gray-800">${info.assignee}</p>
+      </div>
+
+      <div>
+        <p class="text-xs text-gray-500">Priority:</p>
+        <span class="inline-block px-3 py-1 text-xs font-semibold ${info.priority === "high" ? "text-purple-500 bg-purple-100" : 
+        info.priority === "medium" ? "text-orange-500 bg-orange-100" : 
+        "text-gray-500 bg-gray-100"}">
+         ${info.priority}
+        </span>
+      </div>
+    </div>
+  </div>`
+modal.showModal()
 }
 
-const displayModal = (info) => {
-  document.getElementById('modalTitle').innerText = info.title;
-  document.getElementById('modalDescription').innerText = info.description;
-  document.getElementById('modalStatus').innerText = info.status;
-  document.getElementById('modalAuthor').innerText = info.author;
-  document.getElementById('modalPriority').innerText = info.priority;
-  document.getElementById('modalDate').innerText = new Date(info.createdAt).toLocaleDateString();
-
-    modal.showModal();
-}
-
-function createLabels(labels) {
-  if (!labels) return "";
+function createLabels(labels){
+  
   return labels.map(label => {
+
     let bgColor = "";
     let textColor = "";
-    let icon = ""
-    if (label.toLowerCase() === "bug") {
+    let icon = "";
+
+    if(label === "bug"){
+      bgColor = "bg-red-100";
+      textColor = "text-red-500";
       icon = '<i class="fa-solid fa-bug"></i>';
-    
-    
     }
-    
-    else if (label.toLowerCase() === "help wanted") {
-      bgColor = "bg-orange-100"; textColor = "text-orange-500";
+    else if(label === "help wanted"){
+      bgColor = "bg-orange-100";
+      textColor = "text-orange-500";
       icon = '<i class="fa-solid fa-life-ring"></i>';
-    } 
-    
-    else {bgColor = "bg-purple-100"; textColor = "text-purple-500";
+    }
+    else {
+      bgColor = "bg-purple-100";
+      textColor = "text-purple-500";
       icon = '<i class="fa-regular fa-star"></i>';
     }
-    
+
     return `
-      <span class="flex items-center gap-1 ${bgColor} ${textColor} text-[10px] px-2 py-0.5 rounded-full font-medium">
+      <span class="flex items-center gap-1 ${bgColor} ${textColor} text-xs px-3 py-1 rounded-full font-medium">
         ${icon} ${label.toUpperCase()}
       </span>
     `;
-    }).join("");
+  }).join(""); 
 }
 
 const loadIssue = () => {
   showSpinner()
   const url = "https://phi-lab-server.vercel.app/api/v1/lab/issues";
   fetch(url)
-  .then((res) => res.json())
-  .then((data) => {
-    allIssue = data.data; 
-    displayIssue(allIssue);
-    hideSpinner()});
+    .then((res) => res.json())
+    .then((issue) => {
+      allIssue = issue.data
+      displayIssue(allIssue);
+      hideSpinner()
+     
+    });
 };
+const displayIssue = (allIssue) => {
+  issueContainer.innerHTML = "";
 
-const displayIssue = (issues) => {
-    issueContainer.innerHTML = "";
-    const countHeader = document.querySelector('.text-3xl.font-bold');
-    if (countHeader) countHeader.innerText = issues.length + " Issues";
+issueCount.innerText = allIssue.length + " Issues"
+ 
+  allIssue.forEach((cards) =>{
+    const cardLavel = createLabels(cards.labels)
+    const card = document.createElement("div");
+    card.className = ` max-w-sm bg-white rounded-xl shadow-md p-5 border-t-4 ${cards.status === "open"? "border-green-500" : "border-purple-500"}
+        `;
 
-    issues.forEach((cards) => {
-        const cardLabel = createLabels(cards.labels)
-        const card = document.createElement("div");
-        
-       
-        const borderColor = cards.status.toLowerCase() === "open" ? "border-green-500" : "border-purple-500";
-
-        card.className = `max-w-sm bg-white rounded-xl shadow-md p-5 border-t-4 ${borderColor} cursor-pointer`;
     card.innerHTML = `
-   
-  <div class="flex justify-between items-center mb-4">
-    
-
-    <div class="w-10 h-10 flex items-center justify-center rounded-full  ${cards.priority === "low" ? "bg-red-100": "bg-green-100" }">
-    ${cards.priority === "high" || cards.priority === "medium" ? '<img class="w-8" src="./assets/Open-Status.png" alt="open">':'<img src="./assets/Closed- Status .png" alt="closed">'}
+  <div onclick="loadModal(${cards.id})" class="flex justify-between items-center mb-4 cursor-pointer">
+    <div class="w-10 h-10 flex items-center justify-center rounded-full  ${cards.status === "closed" ? "bg-red-100": "bg-green-100" }">
+    ${cards.status === "open"? '<img class="w-8" src="./assets/Open-Status.png" alt="open">':'<img src="./assets/Closed-Status.png" alt="">'}
       
     </div>
-    <span class=" text-sm px-4 py-1 rounded-full font-semibold ${cards.priority === "high" ? "text-red-500 bg-red-100" : 
+    <span class=" text-sm px-4 py-1 rounded-full font-semibold ${cards.priority === "high" ? "text-purple-500 bg-purple-100" : 
         cards.priority === "medium" ? "text-orange-500 bg-orange-100" : 
         "text-gray-500 bg-gray-100"}">
     ${cards.priority}
     </span>
   </div>
-
-
-  <h2 class="text-lg font-bold text-gray-800 mb-2">
+  <h2 onclick="loadModal(${cards.id})" class="text-lg font-bold text-gray-800 mb-2 cursor-pointer">
     ${cards.title}
   </h2>
-
- 
-  <p class="text-gray-500 text-sm mb-4 line-clamp-2">
+  <p onclick="loadModal(${cards.id})" class="text-gray-500 text-sm mb-4 line-clamp-2 ">
     ${cards.description}
   </p>
-
-
-  <div class="flex gap-3 mb-4">
-    <span class="flex items-center gap-1 bg-red-100 text-red-500 text-xs px-3 py-1 rounded-full font-medium">
-     <i class="fa-solid fa-bug  "></i>
-      BUG
-    </span>
-
-    <span class="flex items-center gap-1 bg-orange-100 text-orange-500 text-xs px-3 py-1 rounded-full font-medium">
-    <i class="fa-solid fa-life-ring"></i>
-      HELP WANTED
-    </span>
+  <div onclick="loadModal(${cards.id})" class="flex gap-3 mb-4 cursor-pointer">
+   ${cardLavel}
   </div>
 
-  <div class="border-t pt-3 text-sm text-gray-500">
+  <div onclick="loadModal(${cards.id})" class="border-t pt-3 text-sm text-gray-500 cursor-pointer">
     <p>#1 ${cards.author}</p>
     <p>${cards.createdAt}</p>
   </div>
-
-
     `;
-
     issueContainer.appendChild(card);
-  
+    
   });
+ 
 };
 loadIssue();
